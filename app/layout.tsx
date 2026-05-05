9fe2269bff40e44f652997bf38e1347da1b878bb
mahmoudzanoun13 <mahmoudzanoun35@yahoo.com>
2026-05-05 07:06:04 +0300

    chore: setup shadcn with rtl, dark support and add all needed components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { DirectionProvider } from "@/components/ui/direction";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +28,18 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <DirectionProvider direction="rtl">{children}</DirectionProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
