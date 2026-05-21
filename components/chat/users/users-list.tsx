3fc26b12f6397e5fb55772c4f2d44312ab3e784e
mahmoudzanoun13 <mahmoudzanoun35@yahoo.com>
2026-05-21 10:44:42 +0300

    feat(ui): replace mock users with real data

--- components/chat/users/users-list.tsx
+++ components/chat/users/users-list.tsx
@@ -1,98 +1,22 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
 import UserItem from "./components/user-item";
+import { usersQuery } from "@/hooks/queries/users/users";
 
 export type ChatUser = {
-  id: string;
+  id: number;
   name: string;
+  email: string;
+  avatar?: string;
 };
 
-const users: ChatUser[] = [
-  {
-    id: "1",
-    name: "User 1",
-  },
-  {
-    id: "2",
-    name: "User 2",
-  },
-  {
-    id: "3",
-    name: "User 3",
-  },
-  {
-    id: "4",
-    name: "User 4",
-  },
-  {
-    id: "5",
-    name: "User 5",
-  },
-  {
-    id: "6",
-    name: "User 6",
-  },
-  {
-    id: "7",
-    name: "User 7",
-  },
-  {
-    id: "8",
-    name: "User 8",
-  },
-  {
-    id: "9",
-    name: "User 9",
-  },
-  {
-    id: "10",
-    name: "User 10",
-  },
-  {
-    id: "11",
-    name: "User 11",
-  },
-  {
-    id: "12",
-    name: "User 12",
-  },
-  {
-    id: "13",
-    name: "User 13",
-  },
-  {
-    id: "14",
-    name: "User 14",
-  },
-  {
-    id: "15",
-    name: "User 15",
-  },
-  {
-    id: "16",
-    name: "User 16",
-  },
-  {
-    id: "17",
-    name: "User 17",
-  },
-  {
-    id: "18",
-    name: "User 18",
-  },
-  {
-    id: "19",
-    name: "User 19",
-  },
-  {
-    id: "20",
-    name: "User 20",
-  },
-];
-
-export async function UsersList() {
+export function UsersList() {
+  const { data: users } = useQuery(usersQuery);
   return (
     <div className="flex flex-col h-full w-full max-h-[calc(100vh-357px)] lg:max-h-[calc(100vh-317px)]">
       <div className="flex flex-col gap-2 overflow-y-auto py-4 pe-2">
-        {users.map((user) => (
+        {users?.map((user: ChatUser) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
